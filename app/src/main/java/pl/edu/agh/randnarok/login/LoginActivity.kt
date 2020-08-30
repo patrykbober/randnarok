18ccf158f4322d3943785afc497e5546778fbc46
Unknown <agata.korzeniowska@o2.pl>
2020-08-30 11:12:51 +0200

    Added logo and login layout

--- app/src/main/java/pl/edu/agh/randnarok/login/LoginActivity.kt
+++ app/src/main/java/pl/edu/agh/randnarok/login/LoginActivity.kt
@@ -11,7 +11,7 @@ import pl.edu.agh.randnarok.R
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
         toolbar_title.text = "Sign up / sign in"
     }
 
