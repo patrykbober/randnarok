7c58099228b8607f5f7baa24cba305ecdb000458
Bartosz Ociepka <bociepka1@gmail.com>
2020-08-30 03:35:05 +0200

    downloading events from the server

--- app/src/main/java/pl/edu/agh/randnarok/EventsListActivity.kt
+++ app/src/main/java/pl/edu/agh/randnarok/EventsListActivity.kt
@@ -3,6 +3,7 @@ package pl.edu.agh.randnarok
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -13,9 +14,13 @@ class EventsListActivity : AppCompatActivity() {
     var eventsList = mutableListOf<Event>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView()
+//        setContentView(R.layout.activity_main)
+        getEvents()
     }
 
+    private fun onEventsReady(){
+        Log.d("TAG", eventsList.toString())
+    }
 
     private fun getEvents() {
         eventsList.removeAll{true}
@@ -25,18 +30,23 @@ class EventsListActivity : AppCompatActivity() {
             object : StringRequest(Method.GET, url, Response.Listener<String> { response ->
                 val jsonResponse = JSONObject(response)
                 val eventsArray = jsonResponse.getJSONArray("events")
-//                for (i in 0..departuresArray.length() - 1) {
-//                    val jsonDeparture = departuresArray.getJSONObject(i)
-//                    val displayTime = jsonDeparture.get("display_time").toString()
-//                    val lineNumber = jsonDeparture.get("line_number").toString()
-//                    val direction = jsonDeparture.get("direction").toString()
-//                    val category = jsonDeparture.get("category").toString()
-//                    val delay = jsonDeparture.get("delay").toString()
-//                    val departure = Departure(displayTime, lineNumber, direction, category, delay)
-//                    departuresList.add(departure)
-//                }
-//                onDeparturesReady()
-            }, Response.ErrorListener {
+                for (i in 0..eventsArray.length() - 1) {
+                    val jsonEvent = eventsArray.getJSONObject(i)
+                    var name = jsonEvent.get("name").toString()
+                    var date = jsonEvent.get("date").toString()
+                    var startTime = jsonEvent.get("startTime").toString()
+                    var endTime = jsonEvent.get("endTime").toString()
+                    var city = jsonEvent.get("city").toString()
+                    var address = jsonEvent.get("address").toString()
+                    var desc = jsonEvent.get("description").toString()
+                    var price = jsonEvent.get("price").toString()
+                    var picture = jsonEvent.get("picture").toString()
+                    val idx = jsonEvent.get("idx").toString().toInt()
+                    val departure = Event(idx, name, date, startTime, endTime, city, address, desc, price, picture)
+                    eventsList.add(departure)
+                }
+                onEventsReady()
+            }, Response.ErrorListener {Log.e("Volley error", it.toString())
             }) {}
         val requestQueue = Volley.newRequestQueue(this)
         requestQueue.add(directionsRequest)
--- app/src/main/java/pl/edu/agh/randnarok/model/Event.kt
+++ app/src/main/java/pl/edu/agh/randnarok/model/Event.kt
@@ -1,4 +1,5 @@
 package pl.edu.agh.randnarok.model
 
-class Event {
+data class Event(var idx :Int,var name:String, var date:String,var startTime:String,var endTime:String,var city:String,var address:String,var desc:String,var price:String,var picture:String) {
+
 }
